591cb01487a812946ed1c17d03a360a3e1393396
Andrea Nemeth <nemandreanemeth@gmail.com>
2017-07-23 14:24:05 +0200

    Comments added.

--- comprehension.py
+++ comprehension.py
@@ -1,33 +1,35 @@
-import random
+# This program comes up with a random number, that the user has to guess in 6 trys.
 
-guessesTaken = 0
+import random  # Imports random module.
 
-print('Hello! What is your name?')
-myName = input()
+guessesTaken = 0  # Assings 0 to guessesTaken variable.
 
-number = random.randint(1, 20)
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+print('Hello! What is your name?')  # Asks for user's name.
+myName = input()  # Assigns user's name to myName variable.
 
-while guessesTaken < 6:
-    print('Take a guess.')
-    guess = input()
-    guess = int(guess)
+number = random.randint(1, 20)  # Assigns a random number between 1 and 20 to number variable.
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')  # Prints.
 
-    guessesTaken += 1
+while guessesTaken < 6:  # While loop that goes as long as guessesTaken is less than 6.
+    print('Take a guess.')  # Asks for user to guess the number.
+    guess = input()  # Assigns user input to guess vriable.
+    guess = int(guess)  # Converts guess into integer.
 
-    if guess < number:
-        print('Your guess is too low.')
+    guessesTaken += 1  # Adds 1 to guessesTaken.
 
-    if guess > number:
-        print('Your guess is too high.')
+    if guess < number:  # If guess is lower than number.
+        print('Your guess is too low.')  # Prints.
 
-    if guess == number:
-        break
+    if guess > number:  # If guess is higher than number.
+        print('Your guess is too high.')  # Prints.
 
-if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+    if guess == number:  # If guess is equal to number.
+        break  # Breaks out of the while loop.
 
-if guess != number:
-    number = str(number)
-    print('Nope. The number I was thinking of was ' + number)
+if guess == number:  # If guess is same as number.
+    guessesTaken = str(guessesTaken)  # Converts guessesTaken to string.
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')  # Prints congratulations message with user's name and number of guesses taken.
+
+if guess != number:  # If guess does not match number.
+    number = str(number)  # Converts number to string.
+    print('Nope. The number I was thinking of was ' + number)  # Prints message with the number the user didn't manage to guess.
